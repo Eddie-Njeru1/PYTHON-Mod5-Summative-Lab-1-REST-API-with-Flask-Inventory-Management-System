2b12aa228eac29fd2893242c83405e1986a1f6fe
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-08-16 19:53:02 +0300

    "Generated openfoodfacts.py which gets data from external API the wired and updated app.py to match."

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-#Flask routes REST API to manage the inventory 
+#Flask routes REST API to manage the inventory in-memory
 
 from flask import Flask, jsonify, request #import dependencies 
 import data #import form data.py
+import openfoodfacts #from openfoodfacts.py
 
 app = Flask(__name__) #Flask application instance
 
@@ -18,7 +19,7 @@ def get_inventory_item(item_id):
         return jsonify({"error": "Item not found"}), 404  
     return jsonify(item), 200 #JSON response with inventory item and a HTTP status code 200 OK
 
-#Create new item in inventory 
+#Create new item in inventory manually
 @app.route('/inventory', methods=['POST']) 
 def create_inventory_item():
     new_data = request.get_json() #Extract JSON data from request body
@@ -26,6 +27,15 @@ def create_inventory_item():
         return jsonify({"error": "Request body must be JSON"}), 400 #JSON response if request body is invalid and a HTTP status code 400
     new_item = data.add_item(new_data) #Pass data to data module to create new item
     return jsonify(new_item), 201 #JSON response with new inventory item and a HTTP status code 201
+
+#Add item in inventory using barcode (from openfoodfacts.py)
+@app.route('/inventory/barcode/<barcode>', methods=['POST'])
+def add_item_by_barcode(barcode): 
+    product_data = openfoodfacts.get_product_by_barcode(barcode) #Retrieve product info from API using barcode
+    if product_data is None:
+        return jsonify({"error": "Product not found in OpenFoodFacts"}), 404 #Return HTTP status code 404 if not found
+    new_item = data.add_item(product_data) #Add product info into inventory
+    return jsonify(new_item), 201 #response with newly created item and HTTP status code 201
 
 #Update existing item in inventory
 @app.route('/inventory/<int:item_id>', methods=['PATCH']) 
